77f4c7a964a3a9b55f0a3788fd120afea5771dfe
Shrey Malhotra <shreks7@gmail.com>
2016-07-18 20:49:41 +0530

    Shrey's Branch

--- src/client/script.ts
+++ src/client/script.ts
@@ -1,16 +1,19 @@
-declare const SimpleWebRTC;
+const origin = "https://192.168.1.13:8443";
+let io = require("socket.io-client");
+let easyrtc = require("easyrtc");
 
-let webrtc = new SimpleWebRTC({
-    // the id/element dom element that will hold "our" video
-    localVideoEl: 'localVideo',
-    // the id/element dom element that will hold remote videos
-    remoteVideosEl: 'remoteVideos',
-    // immediately ask for camera access
-    autoRequestMedia: true
-});
+let selfEasyrtcid = "";
+let socket = io.connect(origin);
 
-// we have to wait until it's ready
-webrtc.on('readyToCall', () => {
-    // you can name it anything
-    webrtc.joinRoom('yoco');
-});
+function my_init() {
+    easyrtc.setRoomOccupantListener(roomListener);
+    easyrtc.easyApp("UrgeTest", "self", ["caller"],
+        function(myId) {
+            console.log("My easyrtcid is " + myId);
+        }
+    );
+}
+
+function roomListener (roomName, otherPeers) {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,77 @@
 const electron = require("electron");
+const Positioner = require("electron-positioner");
 
-const app = electron.app;
+const {app, Menu, Tray} = electron;
+const {BrowserWindow} = electron;
+const {ipcMain} = electron;
 
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+let parentWindow;
 let mainWindow;
 
-function createWindow() {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+const mainWindowTransform = {
+    width: 640,
+    height: 480,
+    posX: 0,
+    posY: 0,
+    offsetX: -65,
+    offsetY: -20
+}
 
-    // and load the index.html of the app.
+function createWindow() {
+
+    parentWindow = new BrowserWindow({
+        frame: false,
+        resizable: false,
+        transparent: true,
+        show: false,
+        skipTaskbar: true,
+        useContentSize: true
+    });
+
+    parentWindow.setIgnoreMouseEvents(true);
+
+    let mainWindowOptions = {
+        parent: parentWindow,
+        modal: true,
+        show: true,
+        frame: false,
+        resizable: false,
+        transparent: true,
+        width: mainWindowTransform.width,
+        height: mainWindowTransform.height,
+        alwaysOnTop: false,
+        movable: true,
+        skipTaskbar: false,
+        background: "#00000033"
+    };
+    mainWindow = new BrowserWindow(mainWindowOptions);
+
+    const positioner = new Positioner(mainWindow);
+    positioner.move("bottomRight");
+
+    let pos = mainWindow.getPosition();
+    mainWindowTransform.posX = pos[0];
+    mainWindowTransform.posY = pos[1];
+
     mainWindow.loadURL(`file://${__dirname}/../src/client/index.html`);
 
-    // Open the DevTools.
     mainWindow.webContents.openDevTools();
 
-    // Emitted when the window is closed.
     mainWindow.on("closed", function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
         mainWindow = null;
     });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on("ready", createWindow);
 
-// Quit when all windows are closed.
+
 app.on("window-all-closed", function () {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== "darwin") {
         app.quit();
     }
 });
 
 app.on("activate", function () {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
         createWindow();
     }
