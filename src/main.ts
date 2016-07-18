173357af96e91dfd264c23a7b0c2054f71bfcf29
Aayush Kapoor <xeoneux@icloud.com>
2016-07-18 17:34:20 +0530

    feat: add easyrtc

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ function createWindow() {
     mainWindow = new BrowserWindow({width: 800, height: 600});
 
     // and load the index.html of the app.
-    mainWindow.loadURL(`file://${__dirname}/../src/index.html`);
+    mainWindow.loadURL(`file://${__dirname}/../src/client/index.html`);
 
     // Open the DevTools.
     mainWindow.webContents.openDevTools();
