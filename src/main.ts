67a6921053377fafd2943568d7c26294498e2a4e
Aayush Kapoor <xeoneux@icloud.com>
2016-07-18 16:43:28 +0530

    feat: add index file

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,9 @@ function createWindow() {
     // Create the browser window.
     mainWindow = new BrowserWindow({width: 800, height: 600});
 
+    // and load the index.html of the app.
+    mainWindow.loadURL(`file://${__dirname}/../src/index.html`);
+
     // Open the DevTools.
     mainWindow.webContents.openDevTools();
 
